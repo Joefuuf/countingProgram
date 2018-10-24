a192b7f932eb050c1d876fb82e490516a86bdd8c
jasho9017 <jasho9017@7KWL6Y1.Student.UGDSB.ED>
2018-10-24 10:13:49 -0400

    Action section program from U2A2

--- src/countingprogram/CountingProgram.java
+++ src/countingprogram/CountingProgram.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ Jaffar Shorbaji
+ October 24th 2018
+ This is a program that does simple maffs
+ U2A2
  */
 
 package countingprogram;
